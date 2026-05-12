1d0e7c22c784a68483adf0b3c71820af5e09144c
crazygamer69082-debug <crazygamer69082@gmail.com>
2026-05-12 17:10:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -96,11 +96,11 @@ function App() {
           <h1 id="heroTitle">
             {birthdayReached ? (
               <>
-                Happy Birthday <span className="highlight">[Name]</span> 🎂
+                Happy Birthday <span className="highlight">duhita</span> 🎂
               </>
             ) : (
               <>
-                Counting down to <span className="highlight">[Name]'s</span>{" "}
+                Counting down to <span className="highlight">duhita's</span>{" "}
                 special day 🎂
               </>
             )}
